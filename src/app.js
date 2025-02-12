a9476b5007289e9d1f18347fec8e99dd06cc8a6e
Solod Ihor <solod7002@gmail.com>
2025-02-12 15:15:56 +0200

    files_controller fixed

--- src/app.js
+++ src/app.js
@@ -4,6 +4,7 @@ const bodyParser = require('body-parser');
 const { exec } = require('child_process');
 const swaggerJSDoc = require("swagger-jsdoc");
 const swaggerUi = require("swagger-ui-express");
+const cors = require('cors');
 
 const authRoutes = require('./routes/authRoutes');
 const routes = require('./routes/dbRoutes/routes');
@@ -18,6 +19,7 @@ const app = express();
 dotenv.config();
 connectRedis();
 
+app.use(cors());
 app.use(express.json());
 app.use(bodyParser.json());
 app.use(metricsMiddleware);
